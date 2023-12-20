d0059d8d22120300632d38b36fb966eff2827852
Felipe Costa Rocha da Silva <felipecrs32@gmail.com>
2023-12-20 16:02:00 -0300

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,21 @@
 import { twMerge } from 'tailwind-merge'
 import { Toaster } from 'sonner'
 import { Poppins } from 'next/font/google'
+import { Metadata } from 'next'
 
 import './globals.css'
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600'] })
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Auto Roleta',
+    default: 'Auto Roleta',
+  },
+  icons: {
+    icon: '/images/shared/logo-simple.svg',
+  },
+}
 
 export default function RootLayout({
   children,
