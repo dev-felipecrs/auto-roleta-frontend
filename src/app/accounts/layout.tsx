286cf493c9b4dfda0108613d5dbd7ca6131d29d7
Felipe Costa Rocha da Silva <felipecrs32@gmail.com>
2023-12-21 18:26:57 -0300

    feat: add py

--- src/app/accounts/layout.tsx
+++ src/app/accounts/layout.tsx
@@ -10,7 +10,7 @@ export default async function AccountsLayout({
   return (
     <main className="flex h-full min-h-screen w-full flex-col items-center">
       <Header />
-      <div className="flex w-full flex-grow items-center justify-center px-8">
+      <div className="flex w-full flex-grow items-center justify-center px-8 py-4">
         {children}
       </div>
     </main>
