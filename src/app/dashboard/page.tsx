5b199db0ac804deeedb6e9e2f37b6d564aebfd95
Felipe Costa Rocha da Silva <felipecrs32@gmail.com>
2023-12-22 17:37:34 -0300

    feat: add score

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,42 @@
 import { Layout } from '@/components/shared'
+import { Card, Score } from '@/components/pages/dashboard'
 
 export default function Dashboard() {
   return (
     <Layout>
-      <div></div>
+      <div className="flex flex-col gap-4 px-8 pb-10 pt-6">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4">
+            <Card title="Placar">
+              <Score />
+            </Card>
+
+            <div className="flex gap-4">
+              <Card title="Saldo atual">
+                <div></div>
+              </Card>
+
+              <Card title="BOT">
+                <div></div>
+              </Card>
+            </div>
+          </div>
+
+          <Card title="Rendimento">
+            <div></div>
+          </Card>
+        </div>
+
+        <div className="flex flex-col-reverse gap-4">
+          <Card title="Apostas">
+            <div></div>
+          </Card>
+
+          <Card title="Configurações">
+            <div></div>
+          </Card>
+        </div>
+      </div>
     </Layout>
   )
 }
