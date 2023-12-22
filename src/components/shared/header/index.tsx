f6b9209d4ba1bf7f5082fc955dfa1f5ebc602896
Felipe Costa Rocha da Silva <felipecrs32@gmail.com>
2023-12-22 00:34:50 -0300

    refactor: rename prop name

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -4,20 +4,20 @@ import Image from 'next/image'
 import { Hamburguer } from './hamburguer'
 
 interface HeaderProps {
-  hasHamburguer?: boolean
+  simpleVersion?: boolean
   sidebarIsVisible?: boolean
   handleSidebarVisibilityToggle?(): void
 }
 
 export function Header({
-  hasHamburguer = true,
+  simpleVersion = false,
   sidebarIsVisible,
   handleSidebarVisibilityToggle,
 }: HeaderProps) {
   return (
     <header className="flex h-20 w-full items-center border-b-[1px] border-[#ffffff26] bg-[#1c1d21] px-8 py-4">
       <div className="flex items-center gap-4 sm:gap-0">
-        {hasHamburguer && (
+        {!simpleVersion && (
           <Hamburguer
             sidebarIsVisible={sidebarIsVisible}
             handleSidebarVisibilityToggle={handleSidebarVisibilityToggle}
