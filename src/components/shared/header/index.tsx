aa918759560aee6739756cc7f06ccd058e3bce0e
Felipe Costa Rocha da Silva <felipecrs32@gmail.com>
2023-12-26 19:21:15 -0300

    feat: centralized infos on user mock

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -63,7 +63,7 @@ export function Header({
 
       <div className="flex items-center gap-7">
         <Plan plan={mockUser.plan} />
-        <ConnectWithBroker />
+        {!mockUser.credentials && <ConnectWithBroker />}
       </div>
     </header>
   )
