da2c3ded7f6bcdeffbec439f6c30fb7465046afe
Felipe Costa Rocha da Silva <felipecrs32@gmail.com>
2023-12-26 22:37:08 -0300

    fix: correct performance graphic layout

--- src/components/pages/dashboard/performance.tsx
+++ src/components/pages/dashboard/performance.tsx
@@ -77,7 +77,7 @@ export function Performance() {
 
   return (
     <ResponsiveContainer width="100%" height="100%" minHeight="20rem">
-      <AreaChart data={data} width={200} height={200}>
+      <AreaChart data={data} margin={{ left: -20 }}>
         <defs>
           <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
             <stop stopColor={graphicColor[mockUser.plan]} />
