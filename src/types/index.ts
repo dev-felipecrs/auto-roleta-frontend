bdef81630dd7dbb88eb92e9b257da1c81b853899
Felipe Costa Rocha da Silva <felipecrs32@gmail.com>
2023-12-31 17:56:10 -0300

    feat: add strategy model on prisma schema

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,1 @@
-export * from './roulette'
 export * from './user'
--- src/types/roulette.ts
+++
@@ -1,7 +0,0 @@
-export type Color = 'red' | 'black' | 'green';
-
-export const BETS: Record<Color, number[]> = {
-  red: [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36],
-  black: [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35],
-  green: [0],
-} as const;
