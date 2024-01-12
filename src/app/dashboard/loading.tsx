ac8bc3c7ea7beff2fbbedf5229796dc80f0e23ec
Felipe Persegona | Kepler Digital <felipe.persegona@kepler-digital.dev>
2024-01-12 10:43:47 -0300

    fix: bug

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -4,7 +4,12 @@ import { Grid } from '@/components/pages/dashboard'
 export default function DashboardLoading() {
   return (
     <Layout user={{} as any}>
-      <Grid user={{} as any} isLoading />
+      <Grid
+        user={{} as any}
+        setUser={{} as any}
+        isFetching={false}
+        setIsFetching={{} as any}
+      />
     </Layout>
   )
 }
