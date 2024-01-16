9061e239bd0e63bb6eaab346ca977d6c13858c87
Felipe Costa <felipecrs32@gmail.com>
2024-01-16 10:23:51 -0300

    feat: add href on create pixstrike account button in connect with broker dialog

--- src/components/shared/header/connect-with-broker.tsx
+++ src/components/shared/header/connect-with-broker.tsx
@@ -128,7 +128,7 @@ export function ConnectWithBroker() {
               </Button>
 
               <Link
-                href="#"
+                href="https://pixstrike.com?c=bc1q9z82prqdknpv4ku3"
                 className="flex h-[50px] w-full items-center justify-center rounded-md border border-white text-base font-semibold text-white transition-all hover:opacity-75 active:brightness-90 disabled:cursor-not-allowed disabled:opacity-50"
               >
                 Criar conta na Pixstrike
