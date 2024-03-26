37038aa2acbe381836f9829b8b27167a1839f081
Felipe Costa <felipecrs32@gmail.com>
2024-03-26 19:51:31 -0300

    feat: change strategies names

--- src/constants/strategies.ts
+++ src/constants/strategies.ts
@@ -5,14 +5,14 @@ export const STRATEGIES_NAMES = [
   'Estratégia Rei Roleta',
   'Estratégia Órfãos',
   'Estratégia Polêmica',
-  'Estratégia Matadora',
-  'Estrátegia Zerinho',
+  'Estratégia TicTac',
+  'Estrátegia Vênus',
 ] as const
 
 export const STRATEGIES: Record<StrategyName, Strategy> = {
   'Estratégia Rei Roleta': [[Color.RED, Color.BLACK, Color.BLACK], Color.RED],
   'Estratégia Órfãos': [[Color.BLACK, Color.RED, Color.BLACK], Color.BLACK],
   'Estratégia Polêmica': [[Color.BLACK, Color.BLACK, Color.RED], Color.RED],
-  'Estratégia Matadora': [[Color.RED, Color.RED, Color.BLACK], Color.BLACK],
-  'Estrátegia Zerinho': [[Color.RED, Color.BLACK, Color.BLACK], Color.RED],
+  'Estratégia TicTac': [[Color.RED, Color.RED, Color.BLACK], Color.BLACK],
+  'Estrátegia Vênus': [[Color.RED, Color.BLACK, Color.BLACK], Color.RED],
 } as const
