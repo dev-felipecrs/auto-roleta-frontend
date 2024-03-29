6fc0a59d8d2f8773bce1cc7eb96fb27f732c1dd7
Felipe Costa <felipecrs32@gmail.com>
2024-03-29 14:18:05 -0300

    refactor: change plan luist

---
+++ src/types/plan.ts
@@ -0,0 +1,14 @@
+import { License, Recurrency } from '@prisma/client'
+
+export type Price = 3699 | 6999 | 29999
+
+export type Plan = {
+  name: string
+  price: Price
+  period?: string
+  benefitsIncluded: string[]
+  benefitsNotIncluded: string[]
+  isPopular?: boolean
+  license: License
+  recurrency: Recurrency | undefined
+}
