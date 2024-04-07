77cbd035cc6fe0b9b9d0bcb44f624e0794c7a450
Felipe Costa <felipecrs32@gmail.com>
2024-04-07 00:38:34 -0300

    feat: add strategy

--- src/constants/strategies.ts
+++ src/constants/strategies.ts
@@ -1,5 +1,12 @@
+import { License } from '@prisma/client'
+
 import { Color } from '@/types/strategy'
 import { Strategy, StrategyName } from '@/types'
+
+export type StrategyProps = {
+  strategy: Strategy
+  licenses: License[]
+}
 
 export const STRATEGIES_NAMES = [
   'Estratégia Rei Roleta',
@@ -10,11 +17,29 @@ export const STRATEGIES_NAMES = [
   'Estrátegia Master',
 ] as const
 
-export const STRATEGIES: Record<StrategyName, Strategy> = {
-  'Estratégia Rei Roleta': [[Color.RED, Color.BLACK, Color.BLACK], Color.RED],
-  'Estratégia Órfãos': [[Color.BLACK, Color.RED, Color.BLACK], Color.BLACK],
-  'Estratégia Polêmica': [[Color.BLACK, Color.BLACK, Color.RED], Color.RED],
-  'Estratégia TicTac': [[Color.RED, Color.RED, Color.BLACK], Color.BLACK],
-  'Estrátegia Vênus': [[Color.RED, Color.BLACK, Color.BLACK], Color.RED],
-  'Estrátegia Master': [[Color.BLACK, Color.BLACK, Color.BLACK], Color.BLACK],
+export const STRATEGIES: Record<StrategyName, StrategyProps> = {
+  'Estratégia Rei Roleta': {
+    strategy: [[Color.RED, Color.BLACK, Color.BLACK], Color.RED],
+    licenses: ['trial', 'vip', 'premium'],
+  },
+  'Estratégia Órfãos': {
+    strategy: [[Color.BLACK, Color.RED, Color.BLACK], Color.BLACK],
+    licenses: ['trial', 'vip', 'premium'],
+  },
+  'Estratégia Polêmica': {
+    strategy: [[Color.BLACK, Color.BLACK, Color.RED], Color.RED],
+    licenses: ['vip', 'premium'],
+  },
+  'Estratégia TicTac': {
+    strategy: [[Color.RED, Color.RED, Color.BLACK], Color.BLACK],
+    licenses: ['vip', 'premium'],
+  },
+  'Estrátegia Vênus': {
+    strategy: [[Color.RED, Color.BLACK, Color.BLACK], Color.RED],
+    licenses: ['vip', 'premium'],
+  },
+  'Estrátegia Master': {
+    strategy: [[Color.BLACK, Color.BLACK, Color.BLACK], Color.BLACK],
+    licenses: ['premium'],
+  },
 } as const
