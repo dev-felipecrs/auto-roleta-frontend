bcdbacd32fb38762207d4e4dc25a1db94e16b70a
Guilherme <guilhermehenrique12308@gmail.com>
2024-04-14 10:55:55 -0300

    refactor: replacing error returns with throw new error

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -1,5 +1,9 @@
+import { pricing } from "@/constants/pricing";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
+
+import type { Price } from "@/types/plan";
+import { getSession } from "@/actions";
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 const stripe = new Stripe(stripeSecretKey!);
@@ -13,49 +17,44 @@ export async function POST(req: NextRequest) {
 
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
+  if (!sig || !webhookSecret) {
+    throw new Error("Webhook secret not found.");
+  }
+
   let event: Stripe.Event;
 
   try {
-    if (!sig || !webhookSecret) {
-      return new NextResponse("Webhook secret not found.", { status: 400 });
-    }
-
     event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
-
     console.log(`🔔  Webhook received: ${event.type}`);
   } catch (err: any) {
-    console.log(`❌ Error message: ${err.message}`);
-    return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
+    throw new Error(`Webhook Error: ${err.message}`);
   }
 
-  if (relevantEvents.has(event.type)) {
-    try {
-      switch (event.type) {
-        case "checkout.session.completed":
-          const {
-            data: {
-              object: { amount_total },
-            },
-          } = event;
+  if (!relevantEvents.has(event.type)) {
+    throw new Error(`Unsupported event type: ${event.type}`);
+  }
 
-          console.log(amount_total)
-          break;
-        default:
-          throw new Error("Unhandled relevant event!");
-      }
-    } catch (error) {
-      console.log(error);
-      return new NextResponse(
-        "Webhook handler failed. View your Next.js function logs.",
-        {
-          status: 400,
+  try {
+    switch (event.type) {
+      case "checkout.session.completed":
+        const { data: { object: { amount_total } } } = event;
+
+        if (!amount_total || !pricing[amount_total as Price]) {
+          throw new Error("Invalid price or price not found");
         }
-      );
+
+        const plan = pricing[amount_total as Price]
+
+        // const { user } = await getSession();
+
+        console.log("Plan:", plan);
+        break;
+      default:
+        throw new Error("Unhandled relevant event!");
     }
-  } else {
-    return new NextResponse(`Unsupported event type: ${event.type}`, {
-      status: 400,
-    });
+  } catch (error: any) {
+    throw new Error(`Webhook handler failed: ${error.message}`);
   }
+
   return new NextResponse(JSON.stringify({ received: true }));
 }
