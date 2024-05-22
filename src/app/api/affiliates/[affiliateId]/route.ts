286c925e67a56e1267ef92c9667395ad8ccbbd6a
Guilherme <guilhermehenrique12308@gmail.com>
2024-05-21 22:22:20 -0300

    feat: create route patch for change balance

--- src/app/api/affiliates/[affiliateId]/route.ts
+++ src/app/api/affiliates/[affiliateId]/route.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod'
+
 import { prisma } from '@/config/prisma'
 
 interface Params {
@@ -27,12 +29,64 @@ export async function GET(request: Request, { params }: Params) {
 
     if (!affiliate) {
       return Response.json('Não existe nenhum afilado com este ID', {
-        status: 404,
+        status: 200,
       })
     }
 
     return Response.json(affiliate, {
       status: 200,
+    })
+  } catch (error) {
+    return Response.json(
+      'Um erro inesperado ocorreu, tente novamente mais tarde!',
+      {
+        status: 500,
+      },
+    )
+  }
+}
+
+const patchSchema = z.object({
+  amount: z.coerce.number(),
+})
+
+export async function PATCH(request: Request, { params }: Params) {
+  try {
+    const data = request.json()
+
+    const payload = patchSchema.safeParse(data)
+
+    if (!payload.success) {
+      return Response.json('Os parametros não foram passados corretamentes', {
+        status: 200,
+      })
+    }
+
+    const findAffiliate = await prisma.affiliate.findFirst({
+      where: {
+        affiliateId: params.affiliateId,
+      },
+    })
+
+    if (!findAffiliate) {
+      return Response.json('Não existe nenhum afilado com este ID', {
+        status: 200,
+      })
+    }
+
+    const { amount } = payload.data
+
+    const updateAffiliate = await prisma.affiliate.update({
+      where: {
+        affiliateId: params.affiliateId,
+      },
+      data: {
+        balance: Number(findAffiliate.balance - amount),
+      },
+    })
+
+    return Response.json(updateAffiliate, {
+      status: 204,
     })
   } catch (error) {
     return Response.json(
